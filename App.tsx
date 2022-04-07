7c987712f0090525cd7ba59a94e2731d2a09eb32
yukicoding <hyq1046410512@gmial.com>
2022-04-08 00:21:01 +0800

    feat: add navigator

--- App.tsx
+++ App.tsx
@@ -2,24 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }: any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('Details')}
+        title="click"
+      ></Button>
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }: any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen1</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   const [number, setNumber] = useState<string>('0');
   return (
-    <View style={styles.container}>
-      <Text>Hi aciel</Text>
-      <TextInput
-        value={number}
-        onChangeText={(text) => setNumber(text)}
-        placeholder="please in put"
-        style={{ borderColor: 'black', height: 40 }}
-      ></TextInput>
-      <Button
-        title="click me"
-        onPress={() => Alert.alert('' + number)}
-      ></Button>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
